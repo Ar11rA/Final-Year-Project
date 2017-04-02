7b521e27791fc8154ba7beee9af93455785edecd
Ar11rA <aritra.ghosh2295@yahoo.com>
2017-04-02 17:31:29 +0530

    Modified css

--- testSQL/replica.sh
+++
@@ -1,4 +0,0 @@
-dropdb -U aritraaritra testdb
-createdb -U aritraaritra testdb
-pg_dump -U aritraaritra finalproject -f ./test/SQLreplica.sql
-psql -U aritraaritra -d testdb -f ./testSQL/replica.sql
